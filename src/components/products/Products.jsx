0b5c57b18200d2bc53e2eee65af0a3493bc51f9d
Adaeze-Tochukwu <adarose.tochukwu@gmail.com>
2023-06-10 12:03:03 +0100

    completed products components

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -11,21 +11,23 @@ export default function Products() {
   }, []);
 
   return (
-    <div>
+    <div className="products">
       <Heading
         header1="RECENT PRODUCTS"
         header2="Recent Products"
         para="Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
       />
-      {products.map((product) => (
-        <div key={product.id}>
-          <img src={product.image} alt="" />
-          <h4>{product.nutritions}</h4>
-          <h2>{product.size}</h2>
-          <p>{product.meaning}</p>
-          <button>{product.button}</button>
-        </div>
-      ))}
+      <div className="products__list">
+        {products.map((product) => (
+          <div key={product.id} className="products__list--card">
+            <img src={product.image} alt="" />
+            <h4>{product.nutritions}</h4>
+            <h2>{product.size}</h2>
+            <p>{product.meaning}</p>
+            <button>{product.button}</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
